809d3f777fb78f53f7919aa222101f75b0fc2677
Ananya Prasad <ananyap1801@gmail.com>
2023-07-05 16:21:54 +0530

    googleauth

---
+++ client/src/components/NavBar/styles.js
@@ -0,0 +1,51 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
+
+export default makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 50px',
+  },
+  heading: {
+    color: 'rgba(40, 78, 191)',
+    fontFamily: 'Georgia, serif', // Specify the desired font family here
+    fontStyle: 'none',
+    fontSize: '32px',
+    fontWeight: 'bold',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
+    textDecoration: 'none',
+    '&:hover': {
+      color: 'rgba(40, 78, 191, 0.8)',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
+  },
+  profile: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+}));
